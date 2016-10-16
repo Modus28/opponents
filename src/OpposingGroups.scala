3497515ff3c53283495753083b80cb3f8228eaaf
Daniel <dbg28@case.edu>
2016-10-16 17:04:20 -0400

    Turned OpposingGroups pseudocode to code

--- src/OpposingGroups.scala
+++ src/OpposingGroups.scala
@@ -27,17 +27,15 @@ class OpposingGroups[N] {
     val set: SetWrapper[N] = new SetWrapper[N](Set(wrapper))
     val newPair: Pair[N] = new Pair(set, new SetWrapper())
     addPair(newPair)
+    wrapper
   }
 
   // Algorithm 4 Add Pair to Database - O(1)
-  def addPair(toAdd: Pair[N]): Unit = {
-    /*
-    2: globalGroups   globalGroups + toAdd
-  Algorithm 5 Remove Pairs from Database - O(1)
-1: procedure removePair(toDel : Set[Pair]])
-2: globalPairs   globalGroups ? toDel
-     */
-  }
+  def addPair(toAdd: Pair[N]): Unit = opposingGroups += toAdd
+
+  //Algorithm 5 Remove Pairs from Database - O(1)
+  def removePair(toDel: Set[Pair[N]]): Unit = opposingGroups --= toDel
+
 
   /*
    A Method to allow direct N-type input to oppose and opponent could be added, but they would
@@ -47,32 +45,35 @@ class OpposingGroups[N] {
    */
 
   // Algorithm 2 Oppose: Updates the database with a new Opposition - O(1)
-  def oppose(x: ObjectWrapper[N], y: ObjectWrapper[N]): Boolean = {
-    /*  2: if x; y are not in the same Pair then
-        3: merge(x:getP air(); x:getContainer(); y:getP air(); y:getContainer())
-      4: else
-      5: Report error . Opposition already known*/
+  def oppose(x: ObjectWrapper[N], y: ObjectWrapper[N]): Unit = {
+     if (!(x.getPair equals y.getPair)){
+       merge(x.getPair, x.getContainer, y.getPair, y.getContainer)
+     }
+     else{
+       throw new IllegalArgumentException
+     }
   }
 
-  //Algorithm 3 Merge: Combine Pairs - O(1)
+  //Algorithm 3: Merge: Combine Pairs - O(1)
   def merge(xp: Pair[N], xs: SetWrapper[N], yp: Pair[N], ys: SetWrapper[N]): Unit = {
-    /*
-        2: xs:appendSet(ys)
-3: xp:getOpponentSet(xs).appendSet(ys:getOpponents())
-4: removePair(Set(yp))
-     */
+    xs.appendSet(ys)
+    xp.getOpponentSet(xs).appendSet(ys.getOpponents)
+    removePair(Set(yp))
   }
 
-  // Algorithm 6 Check if ObjectWrappers are Opponents - O(1)
+  // Algorithm 6: Check if ObjectWrappers are Opponents - O(1)
   def opponents(x: ObjectWrapper[N], y: ObjectWrapper[N]): Try[Boolean] = {
-    /* 2: if x: getP air () = y: getP air () then
-     3: if x: getContainer () = y: getContainer () then
-       4: report false
-     5: else
-     6: report true
-     7: else
-     8: report unknown.Can be any status variable*/
-
+    if (x.getPair equals y.getPair) {
+      if (x.getContainer equals y.getContainer) {
+        Success(true)
+      }
+      else {
+        Success(false)
+      }
+    }
+    else {
+      Failure(new NoSuchFieldException)
+    }
   }
 
 
