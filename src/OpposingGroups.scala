316243ef2ae83f86a95a64d9b1b8abbc9a654c88
Daniel <dbg28@case.edu>
2016-10-16 19:00:54 -0400

    Scaladoc'd OpposingGroups

--- src/OpposingGroups.scala
+++ src/OpposingGroups.scala
@@ -1,4 +1,5 @@
-import scala.util.{Try, Success, Failure}
+import scala.collection.mutable
+import scala.util.{Failure, Success, Try}
 
 /** EECS 293: Opposing Groups
   * Daniel Grigsby
@@ -13,27 +14,33 @@ class OpposingGroups[N] {
 
   // Fields
 
-  var opposingGroups: Set[Pair[N]] = ???
+  val opposingGroups: mutable.HashSet[Pair[N]] = new mutable.HashSet[Pair[N]]()
 
   // Methods
 
-  /** Create: Add N to the database inside a new Group - O(1)
+  /** Create: Add x to the database inside a new Group - O(1)
     *
     * @param x the object to add to the known objects list
-    * @return
+    * @return the ObjectWrapper containing the input, x
     */
   def create(x: N): ObjectWrapper[N] = {
     val wrapper: ObjectWrapper[N] = new ObjectWrapper[N](x)
     val set: SetWrapper[N] = new SetWrapper[N](Set(wrapper))
-    val newPair: Pair[N] = new Pair(set, new SetWrapper())
+    val newPair: Pair[N] = new Pair(set, new SetWrapper[N](null))
     addPair(newPair)
     wrapper
   }
 
-  // Algorithm 4 Add Pair to Database - O(1)
+  /** Add Pair to Database - O(1)
+    *
+    * @param toAdd the pair to add to the database
+    */
   def addPair(toAdd: Pair[N]): Unit = opposingGroups += toAdd
 
-  //Algorithm 5 Remove Pairs from Database - O(1)
+  /** Remove Pairs from Database - O(1)
+    *
+    * @param toDel the pairs to remove from the database
+    */
   def removePair(toDel: Set[Pair[N]]): Unit = opposingGroups --= toDel
 
 
@@ -44,7 +51,11 @@ class OpposingGroups[N] {
    as there is zero loss in functionality.
    */
 
-  // Algorithm 2 Oppose: Updates the database with a new Opposition - O(1)
+  /** Oppose: Updates the database with a new Opposition - O(1)
+    *
+    * @param x the object that opposes y
+    * @param y the object that opposes x
+    */
   def oppose(x: ObjectWrapper[N], y: ObjectWrapper[N]): Unit = {
      if (!(x.getPair equals y.getPair)){
        merge(x.getPair, x.getContainer, y.getPair, y.getContainer)
@@ -54,14 +65,26 @@ class OpposingGroups[N] {
      }
   }
 
-  //Algorithm 3: Merge: Combine Pairs - O(1)
+  /** Combine Pairs - O(1)
+    * Adds the values of one pair to another, and removes the original one.
+    *
+    * @param xp The pair that will envelop another pair
+    * @param xs The set in xp that will have a set added to it
+    * @param yp The pair to be merged into xp
+    * @param ys The set in yp that will be added inside xs
+    */
   def merge(xp: Pair[N], xs: SetWrapper[N], yp: Pair[N], ys: SetWrapper[N]): Unit = {
     xs.appendSet(ys)
     xp.getOpponentSet(xs).appendSet(ys.getOpponents)
     removePair(Set(yp))
   }
 
-  // Algorithm 6: Check if ObjectWrappers are Opponents - O(1)
+  /** Check if ObjectWrappers are Opponents - O(1)
+    *
+    * @param x
+    * @param y
+    * @return
+    */
   def opponents(x: ObjectWrapper[N], y: ObjectWrapper[N]): Try[Boolean] = {
     if (x.getPair equals y.getPair) {
       if (x.getContainer equals y.getContainer) {
