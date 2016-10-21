c0eb4f4e56a56aa658e1e6fbc46c3a944a06a6b0
Daniel <dbg28@case.edu>
2016-10-21 00:07:18 -0400

    Added predefined Sets and Pairs to the Testing Helper, tested two methods in SetWrapper

--- src/dbg28/TestTool.scala
+++ src/dbg28/TestTool.scala
@@ -13,26 +13,46 @@ class TestTool {
   // Global fields
 
   var database: OpposingGroups[Ninja] = _
+  var bigDatabase: OpposingGroups[Ninja] = _
   var n1: Ninja = _
   var n2: Ninja = _
   var ninjaSetOfTen: Set[Ninja] = _
   var n1Wrap: ObjectWrapper[Ninja] = _
   var n2Wrap: ObjectWrapper[Ninja] = _
   var objectWrapperSetOfTen: Set[ObjectWrapper[Ninja]] = _
-
+  var setWrapperFirst: SetWrapper[Ninja] = _
+  var setWrapperSecond: SetWrapper[Ninja] = _
+  var setWrapperThird: SetWrapper[Ninja] = _
+  var setWrapperFourth: SetWrapper[Ninja] = _
+  var pairFirst: Pair[Ninja] = _
+  var pairSecond: Pair[Ninja] = _
 
   /**
     *  Setup: Initialize a database with two Pairs of single Ninjas
     */
   def createDatabaseWithNinjas(): TestTool = {
+    // Start new Database
     database = new OpposingGroups[Ninja]()
+    //Initialize Two Ninjas and their ObjectWrappers -- probably can remove
     n1 = new Ninja()
     n2 = new Ninja()
-    ninjaSetOfTen = Set.empty[Ninja]
-    1 to 10 foreach { _ => ninjaSetOfTen + new Ninja() }
     n1Wrap = database.create(n1)
     n2Wrap = database.create(n2)
-    ninjaSetOfTen foreach {objectWrapperSetOfTen + new ObjectWrapper(_)}
+    // Initialize a Set of 10 Ninjas and their set of ObjectWrappers
+    ninjaSetOfTen = Set.empty[Ninja]
+    1 to 10 foreach { _ => ninjaSetOfTen + new Ninja() }
+    ninjaSetOfTen foreach {objectWrapperSetOfTen + bigDatabase.create(_)}
+    // Initialize four SetWrappers
+    setWrapperFirst = new SetWrapper[Ninja]()
+    setWrapperSecond = new SetWrapper[Ninja]()
+    setWrapperThird = new SetWrapper[Ninja]()
+    setWrapperFourth = new SetWrapper[Ninja]()
+    // Initialize two Pairs
+    pairFirst = new Pair[Ninja](setWrapperFirst, setWrapperSecond)
+    List(setWrapperFirst, setWrapperSecond).foreach(_.setPair(pairFirst))
+    pairSecond = new Pair[Ninja](setWrapperThird, setWrapperFourth)
+    List(setWrapperThird, setWrapperFourth).foreach(_.setPair(pairSecond))
+    // TODO: Separate into functionally cohesive methods
     this
   }
 }
