e39c9f4ce213681081a90885e38ac11e9a62cf5d
Daniel <dbg28@case.edu>
2016-10-20 21:52:32 -0400

    Added a helper class to centralize what would need to be repeated code in different tester classes

---
+++ src/dbg28/TestTool.scala
@@ -0,0 +1,33 @@
+package dbg28
+
+/**
+  * EECS 293
+  * Created by Daniel on 10/20/2016.
+  * dbg28@case.edu
+  *
+  * The purpose of this class is to provide other tester classes with
+  * easy access to fresh instances of default data, random data, and other testing data
+  */
+class TestTool {
+
+  // Global fields
+
+  var database: OpposingGroups[Ninja] = _
+  var n1: Ninja = _
+  var n2: Ninja = _
+  var n1Wrap: ObjectWrapper[Ninja] = _
+  var n2Wrap: ObjectWrapper[Ninja] = _
+
+
+  /**
+    *  Setup: Initialize a database with two Pairs of single Ninjas
+    */
+  def createDatabaseWithNinjas(): TestTool = {
+    database = new OpposingGroups[Ninja]()
+    n1 = new Ninja()
+    n2 = new Ninja()
+    n1Wrap = database.create(n1)
+    n2Wrap = database.create(n2)
+    this
+  }
+}
